d250d02e4a00d9422c1ea274275dd0e36d2616fe
Victoria Bezrukova <victoria.bezrukova@duallab.com>
2021-01-25 14:08:37 +0300

    Add ColorContrastChecker prototype

---
+++ src/main/java/org/verapdf/wcag/algorithms/semanticalgorithms/ContrastRatioChecker.java
@@ -0,0 +1,167 @@
+package org.verapdf.wcag.algorithms.semanticalgorithms;
+
+import org.verapdf.wcag.algorithms.entities.enums.TextType;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.*;
+import java.util.List;
+
+public class ContrastRatioChecker {
+
+	public boolean isTextContrastRatioCompliant(BufferedImage sourceTextImage) {
+		return isTextContrastRatioCompliant(sourceTextImage, TextType.REGULAR);
+	}
+
+	public boolean isTextContrastRatioCompliant(BufferedImage sourceTextImage, TextType type) {
+		return isTextContrastRatioCompliant(sourceTextImage, type, false);
+	}
+
+	public boolean isTextContrastRatioCompliant(BufferedImage sourceTextImage, TextType type, boolean isHighVisibility) {
+		List<DataPoint> localMaximums = findLocalMaximums(getLuminosityPresenceList(sourceTextImage));
+		double[] contrastColors = get2MostPresentElements(localMaximums);
+		double colorContrast = getContrastRatio(contrastColors[0], contrastColors[1]);
+		return isContrastRatioCompliant(colorContrast, type, isHighVisibility);
+	}
+
+	public boolean isContrastRatioCompliant(double colorContrast, TextType type, boolean isHighVisibility) {
+		switch (type) {
+			case REGULAR:
+				return isHighVisibility ? colorContrast >= 7.0 : colorContrast >= 4.5;
+			case LARGE:
+				return isHighVisibility ? colorContrast >= 4.5 : colorContrast >= 3.0;
+			case LOGO:
+				return true;
+			default:
+				break;
+		}
+		return false;
+	}
+
+	public double getContrastRatio(BufferedImage sourceTextImage) {
+		List<DataPoint> localMaximums = findLocalMaximums(getLuminosityPresenceList(sourceTextImage));
+		double[] contrastColors = get2MostPresentElements(localMaximums);
+		return getContrastRatio(contrastColors[0], contrastColors[1]);
+	}
+
+	public double getContrastRatio(double first, double second) {
+		double l1 = Math.max(first, second);
+		double l2 = Math.min(first, second);
+		return (l1 + 0.05) / (l2 + 0.05);
+	}
+
+	private double relativeLuminosity(Color color) {
+		double normalizedRed = normalizeColorComponent(color.getRed());
+		double normalizeGreen = normalizeColorComponent(color.getGreen());
+		double normalizeBlue = normalizeColorComponent(color.getBlue());
+		return 0.2126 * normalizedRed + 0.7152 * normalizeGreen + 0.0722 * normalizeBlue;
+	}
+
+	private double normalizeColorComponent(int colorComponent) {
+		double doubleColorComponent = colorComponent / 255.;
+		return doubleColorComponent < 0.03928 ? doubleColorComponent / 12.92 :
+		       Math.pow(((doubleColorComponent + 0.055) / 1.055), 2.4);
+	}
+
+	private List<DataPoint> getLuminosityPresenceList(BufferedImage bim) {
+		int width = bim.getWidth();
+		int height = bim.getHeight();
+		Map<Color, DataPoint> colorMap = new HashMap<>();
+
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				int rgb = bim.getRGB(i, j); //always returns TYPE_INT_ARGB
+				int alpha = (rgb >> 24) & 0xFF;// not used for now, should remove?
+				int red = (rgb >> 16) & 0xFF;
+				int green = (rgb >> 8) & 0xFF;
+				int blue = (rgb) & 0xFF;
+				Color color = new Color(red, green, blue);
+
+				if (colorMap.containsKey(color)) {
+					colorMap.get(color).totalOccurrence++;
+				} else {
+					double relativeLuminosity = relativeLuminosity(color);
+					colorMap.put(color, new DataPoint(relativeLuminosity));
+				}
+			}
+		}
+
+		return new ArrayList<>(new TreeSet<>(colorMap.values()));
+	}
+
+	private List<DataPoint> findLocalMaximums(List<DataPoint> source) {
+		List<DataPoint> localMaximums = new ArrayList<>();
+		boolean isPreviousLessThanCurrent = true;
+
+		// Iterating over all points to check
+		// local maximum
+		for (int i = 0; i < source.size() - 1; i++) {
+			// Condition for local maximum
+			boolean isNextLessThanCurrent = source.get(i).totalOccurrence > source.get(i + 1).totalOccurrence;
+			if (isNextLessThanCurrent && isPreviousLessThanCurrent) {
+				localMaximums.add(source.get(i));
+			}
+			isPreviousLessThanCurrent = !isNextLessThanCurrent;
+		}
+
+		// Checking whether the last point is
+		// local maximum or none
+		if (isPreviousLessThanCurrent) {
+			localMaximums.add(source.get(source.size() - 1));
+		}
+
+		return localMaximums;
+	}
+
+	private double[] get2MostPresentElements(List<DataPoint> source) {
+		double absoluteMaxPresent = -1;
+		double secondMaxPresent = -1;
+		int max = 0;
+		int secondMax = 0;
+
+		for (DataPoint dataPoint: source) {
+			if (dataPoint.totalOccurrence >= max) {
+				secondMaxPresent = absoluteMaxPresent;
+				secondMax = max;
+				absoluteMaxPresent = dataPoint.value;
+				max = dataPoint.totalOccurrence;
+			} else if (dataPoint.totalOccurrence >= secondMax) {
+				secondMax = dataPoint.totalOccurrence;
+				secondMaxPresent = dataPoint.value;
+			}
+		}
+		return new double[]{absoluteMaxPresent, secondMaxPresent};
+	}
+
+	static class DataPoint implements Comparable<DataPoint> {
+
+		private double value;
+		private int totalOccurrence;
+
+		public DataPoint() {
+		}
+
+		public DataPoint(double value) {
+			this.value = value;
+			this.totalOccurrence = 1;
+		}
+
+		public double getValue() {
+			return value;
+		}
+
+		public int getTotalOccurrence() {
+			return totalOccurrence;
+		}
+
+		public void setTotalOccurrence(int totalOccurrence) {
+			this.totalOccurrence = totalOccurrence;
+		}
+
+		@Override
+		public int compareTo(DataPoint o) {
+			return Double.compare(this.value, ((DataPoint) o).value);
+		}
+	}
+
+}
---
+++ src/test/java/org/verapdf/wcag/algorithms/semanticalgorithms/ContrastRatioCheckerTests.java
@@ -0,0 +1,161 @@
+package org.verapdf.wcag.algorithms.semanticalgorithms;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.ImageType;
+import org.apache.pdfbox.rendering.PDFRenderer;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.statistics.HistogramDataset;
+import org.jfree.data.statistics.HistogramType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.verapdf.wcag.algorithms.entities.enums.TextType;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ContrastRatioCheckerTests {
+
+	static Stream<Arguments> contrastTestPassParams() {
+		return Stream.of(
+				Arguments.of("/files/colorcontrast/1.4.3-t01-pass-a.pdf", new int[]{235, 285, 380, 87}, TextType.LARGE),
+				Arguments.of("/files/colorcontrast/1.4.3-t02-pass-a.pdf", new int[]{235, 235, 245, 57}, TextType.REGULAR),
+				Arguments.of("/files/colorcontrast/1.4.3-t03-pass-a.pdf", new int[]{235, 525, 390, 62}, TextType.LOGO)
+		                );
+	}
+
+	static Stream<Arguments> contrastTestFailParams() {
+		return Stream.of(
+				Arguments.of("/files/colorcontrast/1.4.3-t01-fail-a.pdf", new int[]{235, 285, 380, 87}, TextType.LARGE),
+				Arguments.of("/files/colorcontrast/1.4.3-t02-fail-a.pdf", new int[]{235, 235, 245, 57}, TextType.REGULAR)
+		                );
+	}
+
+	@ParameterizedTest(name = "{index}: ({0} ({1}) => {0}))")
+	@MethodSource("contrastTestPassParams")
+	public void createImagesFromPdfPdfBoxPassTest(String sourcePath, int[] bBox, TextType type) throws IOException {
+		PDDocument document = PDDocument.load(getClass().getResourceAsStream(
+				sourcePath));
+		PDFRenderer pdfRenderer = new PDFRenderer(document);
+		for (int page = 0; page < document.getNumberOfPages(); ++page) {
+			BufferedImage bim = pdfRenderer.renderImageWithDPI(page, 300, ImageType.RGB);
+			BufferedImage targetBim = bim.getSubimage(bBox[0], bBox[1], bBox[2], bBox[3]);
+
+			ContrastRatioChecker checker = new ContrastRatioChecker();
+
+			Assertions.assertTrue(checker.isTextContrastRatioCompliant(targetBim, type, false));
+		}
+		document.close();
+	}
+
+	@ParameterizedTest(name = "{index}: ({0} ({1} , {2}) => {0}))")
+	@MethodSource("contrastTestFailParams")
+	public void createImagesFromPdfPdfBoxFailTest(String sourcePath, int[] bBox, TextType type) throws IOException {
+		PDDocument document = PDDocument.load(getClass().getResourceAsStream(
+				sourcePath));
+		PDFRenderer pdfRenderer = new PDFRenderer(document);
+		for (int page = 0; page < document.getNumberOfPages(); ++page) {
+			BufferedImage bim = pdfRenderer.renderImageWithDPI(page, 300, ImageType.RGB);
+			BufferedImage targetBim = bim.getSubimage(bBox[0], bBox[1], bBox[2], bBox[3]);
+
+			ContrastRatioChecker checker = new ContrastRatioChecker();
+
+			Assertions.assertFalse(checker.isTextContrastRatioCompliant(targetBim, type, false));
+		}
+		document.close();
+	}
+
+	private List<String> presentListAsHistogram(List<ContrastRatioChecker.DataPoint> source) {
+		List<String> result = new ArrayList<>();
+		int absoluteMax = 0;
+		for (ContrastRatioChecker.DataPoint entry : source) {
+			if (entry.getTotalOccurrence() > absoluteMax) {
+				absoluteMax = entry.getTotalOccurrence();
+			}
+		}
+		for (ContrastRatioChecker.DataPoint entry : source) {
+			StringBuilder stb = new StringBuilder();
+			stb.append(entry.getValue()).append("  - ").append(entry.getTotalOccurrence()).append(" :   ");
+			int relativePointCount = entry.getTotalOccurrence() * 1500 / absoluteMax / 10;
+			for (int i = 0; i < relativePointCount; i++) {
+				stb.append('*');
+			}
+			result.add(stb.toString());
+		}
+		return result;
+	}
+
+	private List<String> presentMapAsPercentHistogram(List<ContrastRatioChecker.DataPoint> source) {
+		List<String> result = new ArrayList<>();
+		int absoluteMax = 0;
+		for (ContrastRatioChecker.DataPoint entry : source) {
+			if (entry.getTotalOccurrence() > absoluteMax) {
+				absoluteMax = entry.getTotalOccurrence();
+			}
+		}
+		int[] percentArray = new int[101];
+		for (ContrastRatioChecker.DataPoint entry : source) {
+			percentArray[(int) Math.round(entry.getValue() * 100)] += entry.getTotalOccurrence();
+		}
+
+		for (int j = 0; j < 101; j++) {
+			percentArray[j] = percentArray[j] * 1000 / absoluteMax / 10;
+		}
+
+		System.out.println("Percent : value 'histogram'");
+		for (int j = 0; j < 101; j++) {
+			StringBuilder stb = new StringBuilder();
+			stb.append(j).append('-').append(j + 1).append(" : ");
+			for (int k = 0; k <= percentArray[j]; k++) {
+				stb.append('*');
+			}
+			result.add(stb.toString());
+		}
+		return result;
+	}
+
+	private int[] presentListAsHistogramData(List<ContrastRatioChecker.DataPoint> source) {
+		int absoluteMax = 0;
+		for (ContrastRatioChecker.DataPoint entry : source) {
+			if (entry.getTotalOccurrence() > absoluteMax) {
+				absoluteMax = entry.getTotalOccurrence();
+			}
+		}
+		int[] percentArray = new int[101];
+		for (ContrastRatioChecker.DataPoint entry : source) {
+			percentArray[(int) Math.floor(entry.getValue() * 100)] += entry.getTotalOccurrence();
+		}
+		return percentArray;
+	}
+
+	private void printHistogramToFile(int[] dataArray, String filePath) throws IOException{
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+		for (int i = 0; i < dataArray.length; i++) {
+			dataset.addValue(dataArray[i], "", String.valueOf(i));
+		}
+		String plotTitle = "Relative luminance";
+		String xaxis = "luminance percent";
+		String yaxis = "pixelsPresent";
+		PlotOrientation orientation = PlotOrientation.VERTICAL;
+		boolean showLegend = false;
+		boolean toolTips = false;
+		boolean urls = false;
+		JFreeChart chart = ChartFactory.createBarChart(plotTitle, xaxis, yaxis,
+		                                               dataset, orientation, showLegend, toolTips, urls);
+		int width = 2500;
+		int height = 1500;
+		ChartUtilities.saveChartAsPNG(new File(filePath), chart, width, height);
+	}
+}
