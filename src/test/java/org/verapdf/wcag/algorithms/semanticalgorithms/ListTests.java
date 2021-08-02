59b4b3ca461cf19c8e11fca2569bc87af859b89c
koalig93 <alexander.kozlov@duallab.com>
2021-08-02 14:30:59 +0300

    added recognition of list ending

--- src/test/java/org/verapdf/wcag/algorithms/semanticalgorithms/ListTests.java
+++ src/test/java/org/verapdf/wcag/algorithms/semanticalgorithms/ListTests.java
@@ -25,7 +25,8 @@ public class ListTests {
         return Stream.of(
                 Arguments.of("list-with-image-label.json", new int[] {3}, false),
                 Arguments.of("NEG-fake-list.json", new int[] {4}, false),
-                Arguments.of("ordered-list1.json", new int[] {5, 5}, true)
+                Arguments.of("ordered-list1.json", new int[] {5, 5}, true),
+                Arguments.of("PDFUA-Ref-2-06_Brochure.json", new int[] {7, 4, 4}, true)
                 );
     }
 
