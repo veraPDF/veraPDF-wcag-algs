9720998a3e13c9b4c172bf33a3cfcdab32ca9c04
Maxim <maxplusim@mail.ru>
2024-03-21 13:03:29 +0300

    Update underline text detection parameters

--- src/main/java/org/verapdf/wcag/algorithms/semanticalgorithms/utils/NodeUtils.java
+++ src/main/java/org/verapdf/wcag/algorithms/semanticalgorithms/utils/NodeUtils.java
@@ -14,7 +14,7 @@ public class NodeUtils {
 
 	public static final double EPSILON = 0.0001;
 	public static final double TABLE_BORDER_EPSILON = 0.011;
-	public static final double[] UNDERLINED_TEXT_EPSILONS = {0.08, 0.3, 0.3, 0.3};
+	public static final double[] UNDERLINED_TEXT_EPSILONS = {0.08, 0.35, 0.3};
 	private static final double[] HEADING_PROBABILITY_PARAMS = {0.3, 0.0291, 0.15, 0.27, 0.1, 0.25, 0.2, 0.5, 0.05, 0.1};
 	private static final double[] HEADING_PROBABILITY_PARAMS_SAME_FONT = {0.55, 0.15, 0.55, 0.4, 0.5, 0.15};
 	private static final double[] HEADING_PROBABILITY_PARAMS_DIFF_FONT = {0.44, 0.1, 0.4, 0.23, 0.35, 0.1};
